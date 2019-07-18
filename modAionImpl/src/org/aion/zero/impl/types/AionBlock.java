bf0ef595663ffd7d19d832612d9a0b85bfc193be
Aayush Rajasekaran <arajasek94@gmail.com>
2019-07-18 15:22:57 -0400

    Add fields for mining, staking, and total difficulty to AbstractBlock

--- modAionImpl/src/org/aion/zero/impl/types/AionBlock.java
+++ modAionImpl/src/org/aion/zero/impl/types/AionBlock.java
@@ -26,7 +26,6 @@ public class AionBlock extends AbstractBlock implements Block {
     /* Private */
     private byte[] rlpEncoded;
     private volatile boolean parsed = false;
-    private BigInteger td = null;
     private A0BlockHeader header;
 
     /* Constructors */
@@ -228,14 +227,6 @@ public class AionBlock extends AbstractBlock implements Block {
         return this.header.getDifficultyBI();
     }
 
-    public BigInteger getCumulativeDifficulty() {
-        if (td == null) {
-            return BigInteger.ZERO;
-        } else {
-            return td;
-        }
-    }
-
     public long getTimestamp() {
         parseRLP();
         return this.header.getTimestamp();
@@ -351,8 +342,8 @@ public class AionBlock extends AbstractBlock implements Block {
         toStringBuff.append("hash=").append(ByteUtil.toHexString(this.getHash())).append("\n");
         toStringBuff.append(header.toString());
 
-        if (td != null) {
-            toStringBuff.append("  cumulative difficulty=").append(td).append("\n");
+        if (totalDifficulty != null) {
+            toStringBuff.append("  cumulative difficulty=").append(totalDifficulty).append("\n");
         }
 
         if (mainChain != null) {
@@ -537,9 +528,5 @@ public class AionBlock extends AbstractBlock implements Block {
         }
         // not an AionBlock encoding
         return null;
-    }
-
-    public void setCumulativeDifficulty(BigInteger _td) {
-        td = _td;
     }
 }
